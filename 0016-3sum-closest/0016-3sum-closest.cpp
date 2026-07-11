28dac26cbacfe3dfba2004f650db63b8edb59c98
Mihir Shekhar Gupta <mihirshekhar887@gmail.com>
2026-07-11 15:57:52 +0530

    Time: 12 ms (43.32%), Space: 14.1 MB (9.81%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -5,29 +5,33 @@ public:
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
-        int closest = nums[0] + nums[1] + nums[2];
+        int ans = nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i < n - 2; i++) {
 
-            int left = i + 1;
-            int right = n - 1;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            while (left < right) {
+            int l = i + 1;
+            int r = n - 1;
 
-                int sum = nums[i] + nums[left] + nums[right];
+            while (l < r) {
 
-                if (abs(target - sum) < abs(target - closest))
-                    closest = sum;
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (abs(target - sum) < abs(target - ans))
+                    ans = sum;
+
+                if (sum == target)
+                    return target;
 
                 if (sum < target)
-                    left++;
-                else if (sum > target)
-                    right--;
+                    l++;
                 else
-                    return sum;
+                    r--;
             }
         }
 
-        return closest;
+        return ans;
     }
 };
