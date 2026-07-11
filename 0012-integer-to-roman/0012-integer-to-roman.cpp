f0fa91f5ef1a15cb24ed23b49f3601bdcc1d81f9
Mihir Shekhar Gupta <mihirshekhar887@gmail.com>
2026-07-11 15:37:47 +0530

    Time: 8 ms (18.78%), Space: 10.9 MB (71.53%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     string intToRoman(int num) {
-
-        vector<int> value = {
-            1000,900,500,400,
-            100,90,50,40,
-            10,9,5,4,1
-        };
-
-        vector<string> symbol = {
-            "M","CM","D","CD",
-            "C","XC","L","XL",
-            "X","IX","V","IV","I"
-        };
-
+        vector<string>w = {"I", "X", "C", "M"};
+        vector<string>w1 = {"V", "L", "D"};
         string ans = "";
-
-        for(int i = 0; i < value.size(); i++) {
-
-            while(num >= value[i]) {
-                ans += symbol[i];
-                num -= value[i];
+        int p=0;
+        while(num>0){
+            int t = num%10;
+            if(t==4||t==9){
+                if(t==4)ans = w[p]+w1[p]+ans;
+                else ans = w[p]+w[p+1]+ans;
+            }else{
+                if(t>=5){
+                    for(int i=0;i<t-5;i++){
+                    ans  = w[p] + ans;
+                    }
+                ans = w1[p]+ans;
+                }else{
+                    for(int i=0;i<t;i++){
+                        ans  = w[p] + ans;
+                    }
+                }
             }
+            p++;
+            num/=10;
         }
-
         return ans;
     }
 };
