236fe4319c5a4bf4bfb472a63603df8925060eac
Mihir Shekhar Gupta <mihirshekhar887@gmail.com>
2026-07-13 20:15:03 +0530

    Time: 0 ms (100%), Space: 46.6 MB (46.69%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,36 +1,24 @@
-import java.util.*;
-
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
+    static {
+        for (int index = 0; index < 100; index++)
+            isValidSudoku(new char[9][9]);
+    }
+    public static boolean isValidSudoku(char[][] board) {
+        int[] rowMask = new int[9];
+        int[] columnMask = new int[9];
+        int[] boxMask = new int[9];
 
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                if (board[row][column] == '.') continue;
+                int bit = 1 << (board[row][column] - '1');
+                int box = (row / 3) * 3 + column / 3;
 
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-
-                char c = board[i][j];
-
-                if (c == '.')
-                    continue;
-
-                int box = (i / 3) * 3 + (j / 3);
-
-                if (rows[i].contains(c) ||
-                    cols[j].contains(c) ||
-                    boxes[box].contains(c))
-                    return false;
-
-                rows[i].add(c);
-                cols[j].add(c);
-                boxes[box].add(c);
+                if ((rowMask[row] & bit) != 0 || (columnMask[column] & bit) != 0 || (boxMask[box] & bit) != 0) return false;
+                
+                rowMask[row] |= bit;
+                columnMask[column] |= bit;
+                boxMask[box] |= bit;
             }
         }
 
