9c507aa329132fcb492da6ac40fb884bad7bf3ee
Mihir Shekhar Gupta <mihirshekhar887@gmail.com>
2026-08-18 16:23:58 +0530

    Time: 3 ms (62.3%), Space: 19.4 MB (35.66%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,18 +1,16 @@
 class Solution:
-    def largestInteger(self, nums: list[int], k: int) -> int:
-        n = len(nums)
-        count = {}
+    def largestInteger(self, nums: List[int], k: int) -> int:
+        # k가 전체 길이와 같으면 무조건 최댓값
+        if k == len(nums):
+            return max(nums)
 
-        for i in range(n - k + 1):
-            seen = set(nums[i:i + k])
-
-            for x in seen:
-                count[x] = count.get(x, 0) + 1
-
-        ans = -1
-
-        for x, freq in count.items():
-            if freq == 1:
-                ans = max(ans, x)
-
-        return ans
+        # k == 1이면 1번만 나온 숫자 중 최댓값
+        if k == 1:
+            arr = [x for x in nums if nums.count(x) == 1]
+        
+        # 위에 해당되지 않으면 nums[0], nums[-1]만 후보임
+        else:
+            arr = [x for x in (nums[0], nums[-1]) if nums.count(x) == 1]
+        
+        # arr 없으면 -1
+        return max(arr) if arr else -1
