6f1eb817bda8468cb0ae44263cd954e7cc127a4d
Mihir Shekhar Gupta <mihirshekhar887@gmail.com>
2026-09-14 11:14:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.29%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.py
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -2,9 +2,24 @@ from typing import List
 
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        
+        #by method 2
+        if (
+            rec1[2] <= rec2[0]      # rec1 is left of rec2
+            or rec1[0] >= rec2[2]   # rec1 is right of rec2
+            or rec1[3] <= rec2[1]   # rec1 is below rec2
+            or rec1[1] >= rec2[3]   # rec1 is above rec2
+        ):
+            return False
+
+        return True
+        
+        #by method 1
+'''
         return (
             max(rec1[0], rec2[0]) < min(rec1[2], rec2[2])
             and
             max(rec1[1], rec2[1]) < min(rec1[3], rec2[3])
         )
-        
+'''       
+
